aae74414e19a73cbc88fc6b1e87c207a94f08dc4
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2017-05-27 18:55:55 -0700

    remove old models, and update answer script

--- model.py
+++ model.py
@@ -16,38 +16,65 @@ df = pd.read_csv("./data/student-mat.csv", sep=";")
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.svm import LinearSVC # Support Vector Machine Classifier model
 
+""" Split Data into Training and Testing Sets """
 def split_data(X, Y):
     return train_test_split(X, Y, test_size=0.3, random_state=17)
 
 """ Confusion Matrix """
 def confuse(y_true, y_pred):
     cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
-    print("\nConfusion Matrix: \n", cm)
+    # print("\nConfusion Matrix: \n", cm)
     fpr(cm)
+    ffr(cm)
 
-""" False Positive Rate """
+""" False Pass Rate """
 def fpr(confusion_matrix):
     fp = confusion_matrix[0][1]
-    tn = confusion_matrix[0][0]
-    rate = float(fp) / (fp + tn)
-    print("\nFalse Positive Rate: ", rate)
+    tf = confusion_matrix[0][0]
+    rate = float(fp) / (fp + tf)
+    print("False Pass Rate: ", rate)
+
+""" False Fail Rate """
+def ffr(confusion_matrix):
+    ff = confusion_matrix[1][0]
+    tp = confusion_matrix[1][1]
+    rate = float(ff) / (ff + tp)
+    print("False Fail Rate: ", rate)
 
     return rate
 
+""" Train Model and Print Score """
+def train_and_score(X, y):
+    X_train, X_test, y_train, y_test = split_data(X, y)
+
+    clf = Pipeline([
+        ('reduce_dim', SelectKBest(chi2, k=2)),
+        ('train', LinearSVC(C=100))
+    ])
+
+    scores = cross_val_score(clf, X, y, cv=5, n_jobs=2)
+    print("Mean Model Accuracy:", np.array(scores).mean())
+
+    clf.fit(X_train, y_train)
+
+    confuse(y_test, clf.predict(X_test))
+    print()
+
+""" Main Program """
 def main():
-    # For each nominal feature, encode to ordinal values
+    print("\nStudent Performance Prediction")
+
+
+    # For each feature, encode to categorical values
     class_le = LabelEncoder()
     for column in df[["school", "sex", "address", "famsize", "Pstatus", "Mjob", "Fjob", "reason", "guardian", "schoolsup", "famsup", "paid", "activities", "nursery", "higher", "internet", "romantic"]].columns:
         df[column] = class_le.fit_transform(df[column].values)
-
-    """ Remove periodic grade features """
-    df.drop(["G1", "G2"], axis = 1, inplace=True)
-    # df.drop(["G1", "G2", "romantic", "Dalc", "sex", "traveltime", "paid", "activities", "nursery", "famsup", "address", "famsize", "schoolsup", "internet", "higher", "school", "Pstatus"], axis=1, inplace=True)
 
     # Encode G3 as pass or fail binary values
     for i, row in df.iterrows():
@@ -56,41 +83,28 @@ def main():
         else:
             df["G3"][i] = 0
 
-    df_orig = df.copy(deep=True)
-
     # Target values are G3
     y = df.pop("G3")
 
     # Feature set is remaining features
     X = df
 
-    print("\nStudent Performance Predictive Modeling")
+    print("\n\nModel Accuracy Knowing G1 & G2 Scores")
+    print("=====================================")
+    train_and_score(X, y)
 
-    ##############################################################################################
+    # Remove grade report 2
+    X.drop(["G2"], axis = 1, inplace=True)
+    print("\n\nModel Accuracy Knowing Only G1 Scores")
+    print("=====================================")
+    train_and_score(X, y)
 
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    # Remove grade report 1
+    X.drop(["G1"], axis=1, inplace=True)
+    print("\n\nModel Accuracy Without Knowing Scores")
+    print("=====================================")
+    train_and_score(X, y)
 
-    clf = Pipeline([
-        ('reduce_dim', SelectKBest(chi2, k=2)),
-        ('train', LinearSVC(C=100))
-    ])
 
-    clf.fit(X_train, y_train)
-    print(clf.score(X_test, y_test))
-
-    confuse(y_test, clf.predict(X_test))
-
-    # params = [
-    #     {
-    #         "kernel": ["rbf", "linear"],
-    #         "C": [1, 10, 100, 1000]
-    #     }
-    # ]
-    #
-    # clf = GridSearchCV(SVC(C=1), params, cv=5)
-    # clf.fit(X_train, y_train)
-    #
-    # k=10, linear, 100: 0.721 mean, 0.118 std
-    # school, absences, reason, schoolsup
 
 main()
